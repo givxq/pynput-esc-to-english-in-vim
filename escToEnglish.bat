a28be866bc816cbc5744daba01c0f186d224f651
david.g <david.g@kakaopaysec.com>
2023-03-19 21:51:09 +0900

    윈도우 실행스크립트 추가

---
+++ escToEnglish.bat
@@ -0,0 +1,3 @@
+@echo off
+PYTHON E:\dev\pynput-esc-to-english-in-vim\escToEnglish.py
+PAUSE
---
+++ escToEnglishRunBackground.bat
@@ -0,0 +1,2 @@
+Set objShell = CreateObject("Shell.Application")
+objShell.ShellExecute "E:\dev\pynput-esc-to-english-in-vim\escToEnglish.bat", "/c lodctr.exe /r" , "", "runas", 0
